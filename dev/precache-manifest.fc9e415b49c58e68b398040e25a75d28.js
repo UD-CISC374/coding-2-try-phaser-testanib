34eb04f3559230e70ae4ec1c69a0798cbd2318ea
Benjamin Testani <testanib@udel.edu>
2020-02-26 16:20:50 -0500

    Updates

---
+++ dev/precache-manifest.fc9e415b49c58e68b398040e25a75d28.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d23bfd0889ace0a80d7c04787d822a4",
+    "url": "/coding-2-try-phaser-testanib/devassets/background.png"
+  },
+  {
+    "revision": "077161e168d87a21d9b3312aff8eaa81",
+    "url": "/coding-2-try-phaser-testanib/devassets/bart.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-testanib/devassets/citations.md"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-testanib/devassets/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-testanib/devassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-testanib/devassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-testanib/devassets/ship3.png"
+  },
+  {
+    "revision": "8e7f0a5f052ef1505fe8194350e0716f",
+    "url": "/coding-2-try-phaser-testanib/devassets/spritesheets/ada.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-testanib/devassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-testanib/devassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-testanib/devassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-testanib/devassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "3f321979c6f1b9791972",
+    "url": "/coding-2-try-phaser-testanib/devgen.main.fa437784cf581cb07906.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-testanib/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "b08a5e7d81923b3f8abec14376fe2a77",
+    "url": "/coding-2-try-phaser-testanib/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-testanib/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-testanib/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-testanib/devprecache-manifest.fc9e415b49c58e68b398040e25a75d28.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
