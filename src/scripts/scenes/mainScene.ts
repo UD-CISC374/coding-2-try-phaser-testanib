0c59b78dae38487b58a139d715b662eb93ba4399
Benjamin Testani <testanib@udel.edu>
2020-02-25 20:43:06 -0500

    changed image files

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -3,12 +3,15 @@ import ExampleObject from '../objects/exampleObject';
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   width: number;
+  height: number;
   background: Phaser.GameObjects.Image;
   ship1: Phaser.GameObjects.Image;
   ship2: Phaser.GameObjects.Image;
   ship3: Phaser.GameObjects.Image;
   ship4: Phaser.GameObjects.Image;
   ship5: Phaser.GameObjects.Image;
+  bart: Phaser.GameObjects.Image;
+  ada: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -18,17 +21,22 @@ export default class MainScene extends Phaser.Scene {
     this.exampleObject = new ExampleObject(this, 0, 0);
     this.background=this.add.image(0,0,"background");
     this.width = 600;
-    this.ship1 = this.add.image(0,Phaser.Math.Between(0, this.width),"ship1");
-    this.ship2 = this.add.image(0,Phaser.Math.Between(0, this.width),"ship2");
-    this.ship3 = this.add.image(0,Phaser.Math.Between(0, this.width),"ship3");
-    this.ship4 = this.add.image(0,Phaser.Math.Between(0, this.width),"ship3");
-    this.ship5 = this.add.image(0,Phaser.Math.Between(0, this.width),"ship2");
+    this.height = 400;
+    this.ship1 = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
+    this.ship2 = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
+    this.ship3 = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
+    this.ship4 = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
+    this.ship5 = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
+    this.bart = this.add.image(0,Phaser.Math.Between(0, this.width),"bart");
     this.background.setOrigin(0,0);
-    this.ship1.setScale(2);
+    this.ship1.setScale(.5);
     this.ship2.setScale(1);
-    this.ship3.setScale(3);
-    this.ship4.setScale(2);
-    this.ship5.setScale(3);
+    this.ship3.setScale(.75);
+    this.ship4.setScale(.5);
+    this.ship5.setScale(.6);
+    this.bart.setScale(1);
+    this.ada = this.physics.add.sprite(this.width / 2 -8, this.height - 64, "ada");
+    //this.ada.
   }
 
   moveShip(ship, speed){
@@ -50,5 +58,6 @@ export default class MainScene extends Phaser.Scene {
     this.moveShip(this.ship3, 3);
     this.moveShip(this.ship4, 2);
     this.moveShip(this.ship5, 3);
+    this.moveShip(this.bart, 2);
   } 
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -8,10 +8,24 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image("ship1", "../assets/ship.png");
     this.load.image("ship2", "../assets/ship2.png");
     this.load.image("ship3", "../assets/ship3.png");
+    this.load.image("bart", "../assets/bart.png");
+    this.load.spritesheet("ada", "../assets/spritesheets/ada.png", {
+      frameWidth: 20,
+      frameHeight: 30
+    });
   }
 
   create() {
     //this.add.text(20,20,"Loading...");
+    this.anims.create({
+      key: "thrust",
+      frames: this.anims.generateFrameNumbers("ada", {
+        start: 0,
+        end: 0
+      }),
+      frameRate: 20,
+      repeat: -1
+    })
     this.scene.start('MainScene');
   }
 }
