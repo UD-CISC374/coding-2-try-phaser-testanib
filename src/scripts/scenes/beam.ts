620368685c24c89481c2a8a3967465a90504ea7d
Benjamin Testani <testanib@udel.edu>
2020-02-27 10:38:47 -0500

    added beam

---
+++ src/scripts/scenes/beam.ts
@@ -0,0 +1,8 @@
+export default class Beam extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene){
+        let x = scene.ada.x;
+        let y = scene.ada.y;
+        super(scene, x, y, "beam");
+        scene.projectiles.add(this);
+    }
+}
